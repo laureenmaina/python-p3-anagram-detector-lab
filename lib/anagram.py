dde6efc608f8a206b686e2c2924cdf76f8f3b66d
Laurine Maina <mainalaureen58@gmail.com>
2024-05-27 07:38:41 +0300

    initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+    
+    def match(self, words):
+        matches = []
+        for examples in words:
+            if sorted(examples) == self.sorted_word:
+                matches.append(examples)
+        return matches
